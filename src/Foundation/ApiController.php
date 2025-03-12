83c727032aaaf03384510fa5198aca63015c3865
binchen <chenbin_2010@sina.com>
2025-03-12 20:31:02 +0800

    update

--- src/Foundation/ApiController.php
+++ src/Foundation/ApiController.php
@@ -2,6 +2,9 @@
 
 namespace Biin2013\DcatAdminTools\Foundation;
 
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\NotFoundExceptionInterface;
 
@@ -15,29 +18,46 @@ class ApiController extends Controller
     protected int $limit = 20;
     protected string $queryField = 'name';
     protected string $queryCondition = 'like';
+    protected bool $paginateResponse = true;
 
     /**
      * @throws ContainerExceptionInterface
      * @throws NotFoundExceptionInterface
      */
-    public function __invoke()
+    public function __invoke(): Collection|LengthAwarePaginator
     {
         $query = request()->get('q');
 
-        $model = $this->resolveWhere(new $this->modelClass, $query);
+        $model = $this->order($this->resolveWhere(new $this->modelClass, $query));
 
-        return $model
-            ->orderBy($this->orderField, $this->order)
-            ->limit($this->limit)
-            ->paginate(null, [$this->idField . ' as id', $this->nameField . ' as text']);
+        return $this->paginateResponse
+            ? $this->paginateResponse($model)
+            : $this->simpleResponse($model);
     }
 
     protected function resolveWhere(Model $model, ?string $query = null)
     {
+        if (!$query) return $model;
+
         if ($this->queryCondition === 'like') {
             $query = '%' . $query . '%';
         }
 
         return $model->where($this->queryField, $this->queryCondition, $query);
+    }
+
+    protected function order(Builder|Model $model)
+    {
+        return $model->orderBy($this->orderField, $this->order);
+    }
+
+    protected function paginateResponse(Builder $model): LengthAwarePaginator
+    {
+        return $model->paginate($this->limit, [$this->idField . ' as id', $this->nameField . ' as text']);
+    }
+
+    protected function simpleResponse(Builder $model): Collection
+    {
+        return $model->limit($this->limit)->get(['id', $this->nameField . ' as text']);
     }
 }
