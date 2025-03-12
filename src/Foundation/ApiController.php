e0376495cadf5db4033b65b68c6f06d7d696ec72
binchen <chenbin_2010@sina.com>
2025-03-12 16:46:23 +0800

    update

--- src/Foundation/ApiController.php
+++ src/Foundation/ApiController.php
@@ -2,25 +2,42 @@
 
 namespace Biin2013\DcatAdminTools\Foundation;
 
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
+
 class ApiController extends Controller
 {
-    protected function selectOptions(
-        $model,
-        string $query = null,
-        string $nameField = 'name',
-        string $idField = 'id',
-        int $limit = 20,
-        string $order = 'desc'
-    )
-    {
-        $query = $query ?? request()->get('q');
+    protected string $modelClass;
+    protected string $nameField = 'name';
+    protected string $idField = 'id';
+    protected string $orderField = 'id';
+    protected string $order = 'desc';
+    protected int $limit = 20;
+    protected string $queryField = 'name';
+    protected string $queryCondition = 'like';
 
-        if ($query) {
-            $model = $model->where($nameField, 'like', '%' . $query . '%');
-        }
+    /**
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    public function __invoke()
+    {
+        $query = request()->get('q');
+
+        $model = $this->resolveWhere(new $this->modelClass, $query);
+
         return $model
-            ->orderBy($idField, $order)
-            ->limit($limit)
-            ->paginate(null, [$idField . ' as id', $nameField . ' as text']);
+            ->orderBy($this->orderField, $this->order)
+            ->limit($this->limit)
+            ->paginate(null, [$this->idField . ' as id', $this->nameField . ' as text']);
+    }
+
+    protected function resolveWhere(Model $model, ?string $query = null)
+    {
+        if ($this->queryCondition === 'like') {
+            $query = '%' . $query . '%';
+        }
+
+        return $model->where($this->queryField, $this->queryCondition, $query);
     }
 }
