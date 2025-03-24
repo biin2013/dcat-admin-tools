574e9b6793e4007564fd4f4cc9e6fac4e4b2a5c1
binchen <chenbin_2010@sina.com>
2025-03-24 22:43:47 +0800

    update

---
+++ src/Foundation/Options.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Biin2013\DcatAdminTools\Foundation;
+
+use Illuminate\Support\Str;
+
+class Options
+{
+    private static function resolveLangPath(): string
+    {
+        $path = array_slice(explode('\\', static::class), 3);
+        $end = array_pop($path);
+
+        if (ends_with($end, 'Options')) {
+            $end = substr($end, 0, -7);
+            $path[] = $end;
+        }
+
+        array_walk($path, fn(&$item) => $item = Str::snake($item));
+
+        return implode('/', $path);
+    }
+
+    protected static function trans(string $path)
+    {
+        return trans('options/' . self::resolveLangPath() . '.' . $path);
+    }
+
+    protected static function response(array $data, $key = null, $default = null)
+    {
+        return $key === null ? $data : ($data[$key] ?? $default);
+    }
+}
