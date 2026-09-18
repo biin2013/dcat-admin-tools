87ece8d24a95cf1b733bc43be5a4f66de0bd63c0
binchen <chenbin_2010@sina.com>
2026-09-19 00:40:27 +0800

    update

--- src/DcatAdminToolsServiceProvider.php
+++ src/DcatAdminToolsServiceProvider.php
@@ -21,6 +21,6 @@ class DcatAdminToolsServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        Admin::script(include './asserts/js/common.js');
+        Admin::script(file_get_contents('./asserts/js/common.js'));
     }
 }
